395a7f0e2bec19b303223610a842bf74a61fe687
Jeremy Kim <jedo@live.unc.edu>
2018-04-23 21:59:46 -0400

    t

--- js/script.js
+++ js/script.js
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
 $(function() {
     $('#canvas').click(function (e) { //Default mouse Position 
         var posX = $(this).offset().left,
@@ -11,31 +8,3 @@ $(function() {
         $('#canvas').append(sprite);
     });
 })
-=======
-function dropHandler(ev) {
-  console.log('File(s) dropped');
-
-  // Prevent default behavior (Prevent file from being opened)
-  ev.preventDefault();
-
-  if (ev.dataTransfer.items) {
-    // Use DataTransferItemList interface to access the file(s)
-    for (var i = 0; i < ev.dataTransfer.items.length; i++) {
-      // If dropped items aren't files, reject them
-      if (ev.dataTransfer.items[i].kind === 'file') {
-        var file = ev.dataTransfer.items[i].getAsFile();
-        console.log('... file[' + i + '].name = ' + file.name);
-      }
-    }
-  } else {
-    // Use DataTransfer interface to access the file(s)
-    for (var i = 0; i < ev.dataTransfer.files.length; i++) {
-      console.log('... file[' + i + '].name = ' + ev.dataTransfer.files[i].name);
-    }
-  } 
-  
-  // Pass event to removeDragData for cleanup
-  removeDragData(ev)
-}
->>>>>>> origin/gh-pages
->>>>>>> e80733d9cb2b9fc5cef59e273e9ae6e6644d7b93
